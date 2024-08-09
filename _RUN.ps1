81959b01708f45aa9bd9fb817660dbb53a7fcea4
digi-ron <32566377+digi-ron@users.noreply.github.com>
2024-08-09 23:53:55 +1000

    improved output formatting

--- _RUN.ps1
+++ _RUN.ps1
@@ -2,7 +2,7 @@
 . "$($PSScriptRoot)\UserExperience.ps1"
 
 # main menu
-$options = @("Get by Diameter", "Get by rim radius", "Get by rim radius & tyre depth")
+$options = @("Get by Diameter", "Get by rim radius", "Get by rim radius & tyre depth", "Get by rim radius & tyre diameter")
 $mainMenuSelection = ShowSelectionDialog -Prompt "Select an option" -Options $options -ShowExitOption $true
 
 switch ($mainMenuSelection) {
@@ -10,13 +10,13 @@ switch ($mainMenuSelection) {
         # diameter calculation
         $diameter = EnterNumber -Prompt "Enter diameter (mm)" -blockZero $true
         $padding = EnterNumber -Prompt "Enter padding (mm)"
-        GetByDiameter -diameter $diameter -paddingmm $padding | Write-Output
+        GetByDiameter -diameter $diameter -paddingmm $padding | Select-Object -Property Size, Diameter | Write-Output
         exit
     }
     2 {
         # rim radius calculation
         $radius = EnterNumber -Prompt "Enter rim radius (0 for all)"
-        GetByRadius $radius | Write-Output
+        GetByRadius $radius | Select-Object -Property Size, Diameter | Write-Output
         exit
     }
     3 {
