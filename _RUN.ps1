4410a1ee4cc8904fc6109b051eebe49a5588f43e
digi-ron <32566377+digi-ron@users.noreply.github.com>
2024-08-09 20:36:02 +1000

    added rim radius switch case

--- _RUN.ps1
+++ _RUN.ps1
@@ -7,9 +7,14 @@ $mainMenuSelection = ShowSelectionDialog -Prompt "Select an option" -Options $op
 
 switch ($mainMenuSelection) {
     1 {
-        $diameter = Read-Host -Prompt "Enter diameter (mm)"
-        $padding = Read-Host -Prompt "Enter padding (mm)"
+        $diameter = EnterNumber -Prompt "Enter diameter (mm)" -blockZero $true
+        $padding = EnterNumber -Prompt "Enter padding (mm)"
         GetByDiameter -diameter $diameter -paddingmm $padding | Write-Output
+        exit
+    }
+    2 {
+        $radius = EnterNumber -Prompt "Enter rim radius (0 for all)"
+        GetByRadius $radius | Write-Output
         exit
     }
 }
