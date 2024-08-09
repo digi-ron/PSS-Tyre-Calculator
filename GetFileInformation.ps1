14f504f23ff6504c213e60ce0ed42de17b388715
digi-ron <32566377+digi-ron@users.noreply.github.com>
2024-08-09 19:20:28 +1000

    split out functionality to multiple functions calling from a single getinformation function

--- GetFileInformation.ps1
+++ GetFileInformation.ps1
@@ -1,29 +1,39 @@
-function GetFileInformation([Parameter(Mandatory=$true)][string]$radius) {
+function GetFileInformation (){
     $parsedFile = Get-Content -Path .\sizes.json | ConvertFrom-Json
     $informationGathered = @()
 
     $radii = ($parsedFile | Get-Member -MemberType NoteProperty).Name
     foreach ($r in $radii) {
         $r = [double]$r
-        if ($radius -eq "0" -or $r -eq $radius) {
-            $sections = ($parsedFile."$($r)" | Get-Member -MemberType NoteProperty).Name
-            foreach ($s in $sections) {
-                $s = [double]$s
-                for($i = 0; $i -lt $parsedFile."$($r)"."$($s)".Count; $i++) {
-                    $a = [double]($parsedFile."$($r)"."$($s)"[$i])
-                    $readableSize = "$($s)/$($a)R$($r)"
-                    #working
-                    $calculatedDiameter = [int][math]::Ceiling(($s * ($a / 100) * 2) + (25.4 * $r))
+        $sections = ($parsedFile."$($r)" | Get-Member -MemberType NoteProperty).Name
+        foreach ($s in $sections) {
+            $s = [double]$s
+            for($i = 0; $i -lt $parsedFile."$($r)"."$($s)".Count; $i++) {
+                $a = [double]($parsedFile."$($r)"."$($s)"[$i])
+                $readableSize = "$($s)/$($a)R$($r)"
+                #working
+                $calculatedDiameter = [int][math]::Ceiling(($s * ($a / 100) * 2) + (25.4 * $r))
 
-                    $informationGathered += [PSCustomObject]@{
-                        Size = $readableSize
-                        Diameter = $calculatedDiameter
-                    }
+                $informationGathered += [PSCustomObject]@{
+                    Width = $s
+                    AspectRatio = $a
+                    RimSize = $r
+                    Size = $readableSize
+                    Diameter = $calculatedDiameter
                 }
             }
         }
     }
+    $output = $informationGathered | Sort-Object -Property Diameter
+    return $output
+}
 
-    $output = $informationGathered | Sort-Object -Property Diameter | Format-Table -AutoSize
+function GetByRadius([Parameter(Mandatory=$true)][int]$radius) {
+    $output = GetFileInformation | Where-Object { $_.RimSize -eq $radius }
+    return $output
+}
+
+function GetByDiameter([Parameter(Mandatory=$true)][int]$diameter, [int]$paddingmm = 0) {
+    $output = GetFileInformation | Where-Object { $_.Diameter -le ($diameter + $paddingmm) -and $_.Diameter -ge ($diameter - $paddingmm) }
     return $output
 }
